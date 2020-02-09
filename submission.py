454c8b5b4164561931f48ba1cb1fa298fc62cd3f
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-02-09 16:42:53 +0000

    prints set of non_terminals and terminals

--- submission.py
+++ submission.py
@@ -10,6 +10,7 @@ import sys
         file_dict: Dictionary containing all input sets and the formula
 '''
 # TODO: Allow splitting by other whitespace types. For now space is fine
+# TODO: Split predicates into symbol and arity tuples
 def read_file(path):
     REQUIRED_FIELDS = set(["variables", "constants", "predicates", "equality", "connectives", "quantifiers", "formula"])
     seen_fields = []
@@ -42,38 +43,57 @@ def read_file(path):
 # If so, this code remains the same for most input files, but replacing the non-terminals containing only terminals(?)
 def print_grammar(fo_dict):
     productions = []
+    non_terminals = []
+    terminals = []
+
+    non_terminals.append("\\form")
+    terminals += [',', '(', ')']
 
     # TODO: Check for empty fo_dict fields
     # First, generate rules for variables
     productions.append(
                 f"\\var -> {' | '.join(fo_dict['variables'])}"
             )
+    non_terminals.append("\\var")
+    terminals += fo_dict['variables']
 
     # Constants
     productions.append(
                 f"\\const -> {' | '.join(fo_dict['constants'])}"
             )
+    non_terminals.append("\\const")
+    terminals += fo_dict['constants'] 
 
     # Predicates
     productions.append(
                 f"\\pred -> {' | '.join(fo_dict['predicates'])}"
             )
+    non_terminals.append("\\pred")
+    terminals += fo_dict['predicates']
 
     # Equality
     productions.append(
                 f"\\eq -> {' | '.join(fo_dict['equality'])}"
             )
+    non_terminals.append("\\eq")
+    terminals += fo_dict['equality']
 
     # Connectives 
     productions.append(
                 f"\\conn -> {' | '.join(fo_dict['connectives'])}"
             )
+    non_terminals.append("\\conn")
+    terminals += fo_dict['connectives']
 
     # Quantifiers
     productions.append(
                 f"\\quan -> {' | '.join(fo_dict['quantifiers'])}"
             )
+    non_terminals.append("\\quan")
+    terminals += fo_dict['quantifiers']
 
+    pprint(non_terminals)
+    pprint(terminals)
     pprint(productions)
 
 if __name__ == "__main__":
