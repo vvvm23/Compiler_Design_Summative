e3b7c870a338b139db6d4dadd30ad7bb75edfa78
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-02-29 17:34:00 +0000

    simple symbol functions

--- submission.py
+++ submission.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from collections import defaultdict
 import matplotlib.pyplot as plt # For visualising graph
 import sys 
 import re
@@ -6,27 +7,61 @@ import re
 class PredictiveParser:
     def __init__(self):
         self.lookahead = None
+        self.string = None
+        self.index = 0
+        self.symbols = defaultdict(list)
+
+    def match(self, c):
+        if self.lookahead == c:
+            self.index += 1
+            self.lookahead = self.string[self.index]
+            return "OK"
+        return "SYNTAX_ERROR"
 
     def formula(self):
         pass
     
     def variable(self):
-        pass
+        variables = self.symbols['variables']
+        for v in variables:
+            if self.lookahead == v:
+                code = self.match(v)
+                break
 
     def constant(self):
-        pass
-
+        constants = self.symbols['constants']
+        for c in constants:
+            if self.lookahead == c:
+                code = self.match(c)
+                break
+        
     def equality(self):
-        pass
+        equalities = self.symbols['equalities']
+        for e in equalities:
+            if self.lookahead == e:
+                code = self.match(e)
+                break
 
     def connective2(self):
-        pass
+        connectives2 = self.symbols['connectives2']
+        for c in connectives2:
+            if self.lookahead == c:
+                code = self.match(c)
+                break
 
     def connective1(self):
-        pass
+        connectives1 = self.symbols['connectives1']
+        for c in connectives1:
+            if self.lookahead == c:
+                code = self.match(c)
+                break
 
     def quantifier(self):
-        pass
+        quantifiers = self.symbols['quantifiers']
+        for q in quantifiers:
+            if self.lookahead == q:
+                code = self.match(q)
+                break
 
     def predicates(self):
         pass
